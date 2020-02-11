b3ba0cbd9d10f5ca48538edfb737827ff559ed40
Karan Kumar <nailedITnerd@gmail.com>
2020-02-11 21:23:21 +0530

    fixed fluid jumbotrons

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 
+posts = [
+	{'author': 'Karan Kumar',
+	'content': 'This is the first blog post',
+	'date': '2/11/2020'},
+
+	{'author': 'Jason Richardson',
+	'content': 'This is the first blog post,This is the first blog post,This is the first blog post,This is the first blog post,This is the first blog post,This is the first blog post,This is the first blog post',
+	'date': '3/11/2020'},
+]
 
 def home(request):
-	return HttpResponse('Home Page for Blog')
+	context = {'posts': posts}
+	return render(request, 'blog/home.html', context)
+
+def about(request):
+	return render(request, 'blog/about.html', {})
+
