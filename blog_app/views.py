9da403f87720ba2dfbec442a95c68a5f5d09b79e
Karan Kumar <nailedITnerd@gmail.com>
2020-02-12 20:05:57 +0530

    changed date format

--- blog_app/views.py
+++ blog_app/views.py
@@ -2,17 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 from . models import Post
 
-posts = [
-	{'author': 'Karan Kumar',
-	'content': 'This is the first blog post',
-	'date': '2/11/2020',
-	'title': 'Random title'},
-
-	{'author': 'Jason Richardson',
-	'content': 'This is the first blog post,This is the first blog post,This is the first blog post,This is the first blog post,This is the first blog post,This is the first blog post,This is the first blog post',
-	'date': '3/11/2020',
-	'title': 'random title'},
-]
 
 def home(request):
 	context = {'posts': Post.objects.all()}
