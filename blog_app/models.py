1607232fb63a24a9ea56e67b487bec7660707940
Karan Kumar <nailedITnerd@gmail.com>
2020-02-12 19:28:25 +0530

    added Post model

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User 
 
-# Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	date = models.DateTimeField(default=timezone.now)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)	
+
+# The User model and the Post model are going to have a relationship (obviously).
+# We will use one(user) to many(posts) relationship.To do this, we will use a foreign key.
+# 'on_delete': tells what to do with Post if affiliated user is deleted.
+# models.CASCADE: tells delete the posts. 
