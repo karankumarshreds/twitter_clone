fe70d77f30a8ac643a100b4b02fdf7a0d5d14c1b
Karan Kumar <nailedITnerd@gmail.com>
2020-02-12 20:02:24 +0530

    added model to views

--- blog_app/models.py
+++ blog_app/models.py
@@ -8,6 +8,9 @@ class Post(models.Model):
 	date = models.DateTimeField(default=timezone.now)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)	
 
+	def __str__(self):
+		return self.title
+
 # The User model and the Post model are going to have a relationship (obviously).
 # We will use one(user) to many(posts) relationship.To do this, we will use a foreign key.
 # 'on_delete': tells what to do with Post if affiliated user is deleted.
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from . models import Post
 
 posts = [
 	{'author': 'Karan Kumar',
@@ -14,7 +15,7 @@ posts = [
 ]
 
 def home(request):
-	context = {'posts': posts}
+	context = {'posts': Post.objects.all()}
 	return render(request, 'blog/home.html', context)
 
 def about(request):
